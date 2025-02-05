b9fb709102691de38a40d14f9fe4cd176ab22230
Bognar Junior <bognar_junior@yahoo.com.br>
2025-02-05 10:10:08 -0300

    Create Component
    Create component SingUp

--- App.tsx
+++ App.tsx
@@ -3,12 +3,14 @@ import { ThemeProvider } from '@shopify/restyle';
 import { theme } from './src/theme/theme';
 import Login from './src/screens/auth/Login';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import SingUp from './src/screens/auth/SingUp';
 
 function App(): React.JSX.Element {
   return (
     <SafeAreaProvider>
       <ThemeProvider theme={theme}>
-        <Login />
+        {/* <Login /> */}
+        <SingUp />
       </ThemeProvider>
     </SafeAreaProvider>
   );
---
+++ src/screens/auth/SingUp/index.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import Screen from '../../../components/Screen';
+import { Text } from '../../../components/Text';
+import TextInput from '../../../components/TextInput';
+import Icon from '../../../components/Icon';
+import Button from '../../../components/Button';
+
+export default function SingUp() {
+  function submitForm() {
+    console.log('submitForm');
+  }
+  return (
+    <Screen>
+      <Text preset="headingLarge" mb="s32">Criar uma conta</Text>
+      <TextInput
+        boxProps={{mb:'s20'}}
+        placeholder="@"
+        label="Usuário"
+      />
+      <TextInput
+        boxProps={{mb:'s20'}}
+        placeholder="Digite seu nome completo"
+        label="Nome Completo"
+      />
+      <TextInput
+        boxProps={{mb:'s20'}}
+        placeholder="Digite seu e-mail"
+        label="E-mail"
+      />
+      <TextInput
+        placeholder="Digite sua senha"
+        label="Senha"
+        RightComponent={<Icon color="gray2" name="eyeOn" />}
+        boxProps={{mb:'s48'}}
+      />
+      <Button
+        onPress={submitForm}
+        title="Criar uma conta"
+        preset="primary"
+        marginTop="s12"
+      />
+    </Screen>
+  );
+}
